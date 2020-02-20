4ec71dc6ab7a09cc4a759b8e8aabdc970a4b24c4
Steven Chapman <steven44@hawaii.edu>
2020-02-20 10:33:10 -1000

    Completed All

--- HackwichFive/ViewController.swift
+++ HackwichFive/ViewController.swift
@@ -10,11 +10,49 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    //created an ibaction to create a series of tasks. The code makes it so the first label will say My Favorite Foods on launch, and the second label is set to cycle through five different food names by using a variable of type Integer with a string attached.
+    @IBOutlet weak var topLabel: UILabel!
+    
+    @IBOutlet weak var bottomLabel: UILabel!
+   
+    @IBOutlet weak var buttonLabel: UIButton!
+    var currentIndex = 0
+    var favoriteFoodArray = ["Pizza", "Burgers", "Steak", "Ribs", "Pasta"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+   self.topLabel.text = "My Favorite Foods"
     }
+   
+    
+   
+    
+    
+    @IBAction func buttonPressed(_ sender: Any) {
+   
+    self.bottomLabel.text =  favoriteFoodArray[currentIndex]
+    
+    if self.currentIndex < self.favoriteFoodArray.count
+    {
+    
+    currentIndex+=1
+    buttonLabel.setTitle("Next", for: UIControl.State.normal)
+    
+    
+    
+    }
+    else
+    {
+    
+    (sender as! UIButton).isEnabled = false
+    
+    }
+    
+    
+}
 
+    
 
 }
 
